69cb42c127096b9d0aef54fafebc0fe3e3182372
Nhahan <kisy324@naver.com>
2021-09-16 16:59:04 +0900

    Go는 func 이름이 대문자로 시작하면 public, 소문자로 시작하면 private

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ksy/learngo/something"
+)
 
-func main() { // Go는 main 패키지의 main function에서 시작. 컴파일을 위해 반드시 있어야함!
+func main() { // Go는 main 패키지의 main func에서 시작. 컴파일을 위해 반드시 있어야함!
 	fmt.Println("Hello world!")
+
+	something.SayHello() // Go는 func 이름이 대문자로 시작하면 public, 소문자로 시작하면 private
+	//something.sayBye() <- 소문자로 시작하므로 private이라 호출 불가
 }
