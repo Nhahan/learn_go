ef49fb3b5b9ccab0e8027320d3258419479b681d
Nhahan <kisy324@naver.com>
2021-09-16 16:53:55 +0900

    Hello world!

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() { // Go는 main 패키지의 main function에서 시작. 컴파일을 위해 반드시 있어야함!
+import "fmt"
 
+func main() { // Go는 main 패키지의 main function에서 시작. 컴파일을 위해 반드시 있어야함!
+	fmt.Println("Hello world!")
 }
