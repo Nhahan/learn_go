ad3aad2753623c604c7c6a987cdeca7747563177
Nhahan <kisy324@naver.com>
2021-09-21 00:33:40 +0900

    method and error

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,35 @@
 package accounts
 
-// Account struct
+import "errors"
+
 type Account struct {
 	owner   string
 	balance int
 }
 
-// NewAccount creates Account
+// NewAccount Constructor function
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Balance method
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Deposit method
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Withdraw method
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+var errNoMoney = errors.New("Can't withdraw")
--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/ksy/learngo/accounts"
+	"log"
 )
 
 func main() {
 	account := accounts.NewAccount("ksy")
 	fmt.Println(account)
+
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance())
 }
