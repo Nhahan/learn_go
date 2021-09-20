0da2c19789967e19b0749d0693971e031822c722
Nhahan <kisy324@naver.com>
2021-09-20 15:47:02 +0900

    making struct and its own constructor

--- main.go
+++ main.go
@@ -2,70 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/ksy/learngo/something"
-	"strings"
+	"github.com/ksy/learngo/accounts"
 )
 
-func multiply(a int, b int) int { // func 타이핑
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) { // return 2개 이상인 func
-	defer fmt.Println("I'm done!") // func 끝난 후에 실행되는 코드 (많이 쓴다고 함)
-	return len(name), strings.ToUpper(name)
-}
-
-func superAdd(numbers ...int) int {
-	fmt.Println(numbers)
-
-	total := 0
-
-	for number := range numbers {
-		total += number
-	}
-	return total
-}
-
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 { // variable expression : koreanAge가 if문의 지역변수임을 알 수 있다
-		return false
-	}
-	return true
-	// variable expression은 switch문에서도 동일하게 사용 가능
-}
-
-type person struct {
-	name    string
-	age     int
-	favFood []string  // slice
-	family  [4]string // array
-}
-
-func main() { // Go는 main 패키지의 main func에서 시작. 컴파일을 위해 반드시 있어야함!
-	fmt.Println("Hello world!")
-
-	something.SayHello() // Go는 func 이름이 대문자로 시작하면 public, 소문자로 시작하면 private
-	//something.sayBye() <- 소문자로 시작하므로 private이라 호출 불가
-
-	const constName string = "ksy" // const 상수
-	var varName string = "ksy"     // var은 변수인데 사용하지 않는 변수는 오류가 뜨고 컴파일이 되지 않음
-	name := "ksy"                  // 축약 변수 선언
-	fmt.Println(varName)
-	fmt.Println(name)
-
-	fmt.Println(multiply(2, 2))
-	totalLength, upperName := lenAndUpper("ksy") // 만약 2개 이상을 리턴하는 func에서 1개만 리턴하고 싶다면 리턴 변수명을 _로 해주면 ignore된다
-	fmt.Println(totalLength, upperName)
-
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
-
-	fmt.Println(canIDrink(16))
-
-	a := 2
-	b := &a
-	fmt.Println(a, *b)                     // *b = 3 -> a = 3
-	favFood := []string{"kimchi", "ramen"} // slice
-	ksy := person{name: "ksy", age: 18, favFood: favFood}
-	fmt.Println(ksy)
+func main() {
+	account := accounts.NewAccount("ksy")
+	fmt.Println(account)
 }
