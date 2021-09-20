428a864eb9dec25b94faccfe728be13c756b72de
Nhahan <kisy324@naver.com>
2021-09-21 01:23:51 +0900

    dictionary / Search, Add, Update method added

--- dict/dict.go
+++ dict/dict.go
@@ -5,8 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That word is already exists")
+var (
+	errNotFound   = errors.New("Not Found")
+	errCantUpdate = errors.New("Cant update non-existing word")
+	errWordExists = errors.New("That word is already exists")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -31,5 +34,16 @@ func (d Dictionary) Add(word, def string) error {
 	//} else if err == nil {
 	//	return errWordExists
 	//}
+	return nil
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
 	return nil
 }
