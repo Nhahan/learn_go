1c7c6fe9083554595d49e4b64331e921c518f51a
Nhahan <kisy324@naver.com>
2021-09-21 01:19:57 +0900

    dictionary / Search, Add method added

--- main.go
+++ main.go
@@ -22,12 +22,20 @@ import (
 
 func main() {
 	dictionary := dict.Dictionary{}
-	dictionary["hello"] = "hello"
 
-	definition, err := dictionary.Search("hello")
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println(hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
 }
