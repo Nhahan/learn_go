a3a76733cdcd8a79811daf335fc27886ebcc2801
Nhahan <kisy324@naver.com>
2021-09-22 19:14:31 +0900

    hitURL

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/ksy/learngo/dict"
+	"net/http"
 )
 
 //// accounts main
@@ -20,22 +21,41 @@ import (
 //	fmt.Println(account.Balance(), account.Owner())
 //}
 
+//func main() {
+//	dictionary := dict.Dictionary{}
+//
+//	word := "hello"
+//	definition := "Greeting"
+//
+//	err := dictionary.Add(word, definition)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	hello, _ := dictionary.Search(word)
+//	fmt.Println(hello)
+//
+//	err2 := dictionary.Add(word, definition)
+//	if err2 != nil {
+//		fmt.Println(err2)
+//	}
+
 func main() {
-	dictionary := dict.Dictionary{}
-
-	word := "hello"
-	definition := "Greeting"
-
-	err := dictionary.Add(word, definition)
-	if err != nil {
-		fmt.Println(err)
-	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println(hello)
-
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
+	urls := []string{
+		"https://www.naver.com/",
+		"https://www.google.com/",
 	}
 
+	for _, url := range urls {
+		fmt.Println(url)
+	}
+}
+
+var errRequestFailed = errors.New("Request failed")
+
+func hitURL(url string) error {
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
