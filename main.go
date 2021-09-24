250e2e88550c72163c653e03709035d274bbb3df
Nhahan <kisy324@naver.com>
2021-09-24 21:51:38 +0900

    goquery

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=%EA%B0%9C%EB%B0%9C%EC%9E%90&l=%EA%B2%BD%EA%B8%B0%EB%8F%84%20%EC%84%B1%EB%82%A8"
+
+func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 2000 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
